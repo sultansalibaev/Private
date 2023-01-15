9298520cb212fa536268fbe5257cdbc84e0e1f9d
Sultan Salibaev <82295225+Doter123@users.noreply.github.com>
2023-01-15 21:12:46 +0600

    Delete add_news_service directory

--- add_news_service/settings.py
+++
@@ -1,9 +0,0 @@
-import json
-import configparser
-
-config = configparser.ConfigParser()
-config.read('connections.ini')
-
-
-class Settings:
-    FRONT_DB_INFO = json.loads(config['CONNECTIONS_INFO']['FRONT_DB'])
